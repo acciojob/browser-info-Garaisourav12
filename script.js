ff11e4cf02cf759fab6413e37d38e5d9a2f6bccc
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2023-10-02 10:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Function to detect the user's browser and display the information
+        function detectBrowser() {
+            // Get browser name and version from the navigator object
+            const browserName = navigator.appName;
+            const browserVersion = navigator.appVersion;
+
+            // Display the information in the "browser-info" div
+            const browserInfoElement = document.getElementById("browser-info");
+            browserInfoElement.textContent = "You are using " + browserName + " version " + browserVersion;
+        }
+
+        // Call the detectBrowser function when the page loads
+        window.onload = detectBrowser;
